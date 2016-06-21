471679076ad73f7c23e23de438e48663b9be2253
Kenneth Buck <kenneth@nestedsphere.com>
2016-06-21 18:46:02 -0400

    remove superfluous object import into initializer

--- addon/initializers/extend-object-with-option.js
+++ addon/initializers/extend-object-with-option.js
@@ -1,5 +1,18 @@
-import object from '../object';
+import Ember from 'ember';
+import option from '../option';
 
 export default function initialize(/* application */) {
-  object();
+
+  Ember.Object.reopen({
+
+    /**
+     * Returns an option Some/None of the value at the given key.
+     *
+     * @param {string} key
+     * @returns {Option}
+     */
+    getAsOption(key){
+      return option(this.get(key));
+    }
+  });
 }
